315cdffa7b0a198542d58291bf2cdc4f1a79d71f
Lars Nielsen <lars@lndesign.dk>
2017-09-22 19:47:50 +0200

    Added intial FCM setup

---
+++ app/src/main/java/dk/lndesign/tunnels/TunnelsInstanceIdService.java
@@ -0,0 +1,9 @@
+package dk.lndesign.tunnels;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Service for device groups and device registration token.
+ */
+public class TunnelsInstanceIdService extends FirebaseInstanceIdService {
+}
---
+++ app/src/main/java/dk/lndesign/tunnels/TunnelsMessagingService.java
@@ -0,0 +1,9 @@
+package dk.lndesign.tunnels;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Message handling with Firebase cloud messaging.
+ */
+public class TunnelsMessagingService extends FirebaseMessagingService {
+}
